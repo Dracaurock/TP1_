b98eea8be62709175519414d74bf34a105cc637d
Dracaurock <153289046+Dracaurock@users.noreply.github.com>
2023-12-08 18:16:17 +0100

    Add files via upload

--- tp1/truc.py
+++ tp1/truc.py
@@ -1,6 +1,4 @@
 import csv
-import matplotlib.pyplot as plt
-
 
 #### fonction ####
 def ouvrir(nom_fichier,sep):                                #cette fonction nous permet d'ouvrir les fichier csv 2008,2016,2021.
@@ -54,7 +52,7 @@ for a in Liste_nom_code:
         if a[0]==(int(b[2])*1000+int(b[5])):
             L2008_3.append(b)
 
-print(L2008_3)
+
 
 L2016=ouvrir("donnees_2016.csv",",")
 L2016_2=[]
@@ -62,9 +60,13 @@ for a in L2016:
     if a[2]=="89":
         L2016_2.append(a)
 
+L2016_3=[]
+for a in Liste_nom_code:
+    for b in L2016_2:
+        if a[0]==(int(b[2])*1000+int(b[5])):
+            L2016_3.append(b)
 
-
-
+print(L2016_3)
 
 L2021=ouvrir("donnees_2021.csv",";")
 L2021_2=[]
@@ -75,13 +77,36 @@ for a in Liste_nom_code:
 
 
 
-pop2008=sommes_pop_années(L2008_2)
-pop2016=sommes_pop_années(L2016_2)
+pop2008=sommes_pop_années(L2008_3)
+pop2016=sommes_pop_années(L2016_3)
 pop2021=sommes_pop_années(L2021_2)
 
-
-
-
+print(pop2008)
+print(pop2016)
+print(pop2021)
 
 ####affichage courbe agglomération totale####
 
+dates=[2008,2016,2021]
+popu2008 = []
+popu2016 = []
+popu2021 = []
+
+popu2008.append(pop2008)
+popu2016.append(pop2016)
+popu2021.append(pop2021)
+
+from matplotlib import pyplot as plt
+
+plt.plot(dates,popu2008,color='b')
+plt.plot(dates,popu2016,color='r')
+plt.plot(dates,popu2021,color='g')
+plt.scatter(dates,popu2008,color='b')
+plt.scatter(dates,popu2016,color='r')
+plt.scatter(dates,popu2021,color='g')
+plt.title("Population à Auxerre agglomération générale")
+plt.ylabel('Population')
+plt.xlabel('Années')
+plt.show()
+
+## DOnc la population décroit bien.
